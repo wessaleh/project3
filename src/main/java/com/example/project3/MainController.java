1703146226738183d3cd7cc3e25ab6f6a18d6dd9
wesamsaleh <wesamssaleh@gmail.com>
2021-10-29 18:42:55 -0400

    Added support for adding resident students

--- src/main/java/com/example/project3/MainController.java
+++ src/main/java/com/example/project3/MainController.java
@@ -8,7 +8,12 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.util.InputMismatchException;
+
 public class MainController {
+
+    private final int MINIMUM_NUM_CREDITS = 3;
+    private final int MAXIMUM_NUM_CREDITS = 24;
 
     @FXML
     private RadioButton ba;
@@ -97,9 +102,118 @@ public class MainController {
         roster = new Roster();
     }
 
+    /**
+     * Gets the selected major
+     * @return - the major selected
+     * @throws InputMismatchException if major has not been selected
+     */
+    private Major getMajor() throws InputMismatchException {
+        if(cs.isSelected())
+            return Major.CS;
+        else if(it.isSelected())
+            return Major.IT;
+        else if(ee.isSelected())
+            return Major.EE;
+        else if(me.isSelected())
+            return Major.ME;
+        else if(ba.isSelected())
+            return Major.BA;
+        else
+            throw new InputMismatchException();
+    }
+
+    /**
+     * Builds the profile for a student
+     * @return student profile
+     */
+    private Profile buildStudentProfile(){
+        String studentName = name.getText();
+        if(studentName.isEmpty()){
+            output.appendText("Student name field is empty. \n");
+            return null;
+        }
+
+        try{
+            Major major = getMajor();
+            return new Profile(studentName, major);
+        }catch (InputMismatchException majorNotSelected){
+            output.appendText("Student major has not been selected \n");
+        }
+
+        return null;
+    }
+
+    /**
+     * Adds a resident student to the roster.
+     * @param profile - the profile of the student to add
+     */
+    public void addResidentStudent(Profile profile){
+        int creditHours = MINIMUM_NUM_CREDITS;
+
+        try {
+            creditHours = Integer.parseInt(credits.getText());
+        }catch(NumberFormatException badInput){
+            output.appendText("Invalid credit hours. \n");
+            return;
+        }
+
+        if(creditHours < MINIMUM_NUM_CREDITS){
+            output.appendText("Minimum credit hours is 3. \n");
+            return;
+        }else if(creditHours > MAXIMUM_NUM_CREDITS){
+            output.appendText("Credit hours exceed the maximum 24. \n");
+            return;
+        }
+
+        Resident residentStudent = new Resident(profile, creditHours);
+        boolean studentAdded = roster.add(residentStudent);
+
+        if(studentAdded){
+            output.appendText("Student added. \n");
+        }else{
+            output.appendText("Student is already in the roster. \n");
+        }
+    }
+
+    public void addNonResidentStudent(Profile profile){
+
+    }
+
+    public void addTriStateStudent(Profile profile){
+
+    }
+
+    public void addInternationalStudent(Profile profile){
+
+    }
+
+    /**
+     * Adds a student to the roster
+     * @param event - the event to be handled
+     */
     @FXML
     void addStudent(ActionEvent event) {
+        Profile studentProfile = buildStudentProfile();
+        if(studentProfile == null){
+            return;
+        }
 
+        boolean isResidentStudent = resident.isSelected();
+        boolean isNonResidentStudent = non_resident.isSelected() &&
+                !tristate.isSelected() && !international.isSelected();
+        boolean isTriStateStudent = tristate.isSelected();
+        boolean isIntlStudent = international.isSelected();
+
+        if(isResidentStudent)
+            addResidentStudent(studentProfile);
+        else if(isNonResidentStudent)
+            addNonResidentStudent(studentProfile);
+        else if(isTriStateStudent)
+            addTriStateStudent(studentProfile);
+        else if(isIntlStudent)
+            addInternationalStudent(studentProfile);
+        else
+            output.appendText("Student status has not been selected \n");
     }
 
     @FXML
